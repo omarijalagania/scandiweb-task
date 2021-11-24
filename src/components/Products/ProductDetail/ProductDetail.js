ebb8e0eae802ebf03419bcceca89c499d9ac1a44
omari <omari.jalagania@gmail.com>
2021-11-24 18:03:55 +0400

    cart and minor changes

--- src/components/Products/ProductDetail/ProductDetail.js
+++ src/components/Products/ProductDetail/ProductDetail.js
@@ -2,22 +2,53 @@ import React, { Component } from "react";
 
 import classes from "./ProductDetail.module.css";
 
+import { connect } from "react-redux";
+import SizeButton from "../../ui/SizeButton";
+
 export class ProductDetail extends Component {
-  componentDidMount() {
-    let { id } = this.props.match.params;
-    console.log(id);
-  }
   render() {
     return (
       <div className={classes.container}>
         <div className={classes.imageContainer}>
-          <div className={classes.smallImages}>small image</div>
-          <div className={classes.bigImage}>big image</div>
+          <div className={classes.smallImages}>
+            <img src="/images/sweater.jpeg" alt="sweater" />
+            <img src="/images/sweater.jpeg" alt="sweater" />
+            <img src="/images/sweater.jpeg" alt="sweater" />
+          </div>
+          <div className={classes.bigImage}>
+            <img src="/images/sweater.jpeg" alt="sweater" />
+          </div>
         </div>
-        <div className={classes.productDetails}>details</div>
+        <div className={classes.productDetailsConteiner}>
+          <h2>Apollo</h2>
+          <h3>Running shorts</h3>
+          <div className={classes.sizes}>
+            <p>size</p>
+            <div className={classes.btnGroup}>
+              <SizeButton className={classes.sizeBtnActive}>XS</SizeButton>
+              <SizeButton>X</SizeButton>
+              <SizeButton>M</SizeButton>
+              <SizeButton>L</SizeButton>
+            </div>
+          </div>
+          <p className={classes.price}>Price</p>
+          <p className={classes.priceAmount}>$50.00</p>
+          <button className={classes.cartBtn}>Cart</button>
+          <p className={classes.description}>
+            dsfsdfdsfsdfsdsdsdfsdsdffsfssks;lflkssfksdkjfsdjflsjf
+            sfsdfsdfsdlfksdflkjsdfjdsjfsdljfsjdlfk
+            skdfslkjdfskljfskljflksjdflkjsfjklsdkfljslkjfsjkf
+          </p>
+        </div>
       </div>
     );
   }
 }
 
-export default ProductDetail;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products,
+  };
+};
+
+export default connect(mapStateToProps)(ProductDetail);
